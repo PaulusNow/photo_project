37be2a7104a1d2096ebbfb0ab3a4469ea40cd326
Dionisius951 <dionisius678@gmail.com>
2025-12-10 10:56:28 +0700

    fix : update docker file and create tables

--- database/migrations/2025_12_10_031828_create_setting_table.php
+++ database/migrations/2025_12_10_031828_create_setting_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('setting', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->unique();
-            $table->string('password');
-            $table->enum('role', ["admin", "user"]);
+            $table->time("open_time");
+            $table->time("close_time");
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('setting');
     }
 };
